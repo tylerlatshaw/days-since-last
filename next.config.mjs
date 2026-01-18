ddd2fd3412c2bff6eb2d9a7b747fa061155f4ccf
Tyler Latshaw <tylerlatshaw@gmail.com>
2026-01-17 23:32:20 -0500

    Added Sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig);
